d32033b2abad230075e04f9daed1afc4a9c34ee8
Heuersv <heuersv@mathematik.uni-marburg.de>
2020-12-07 20:09:07 +0100

    Added comments

--- test_setup.py
+++ test_setup.py
@@ -5,20 +5,29 @@ from functions import doppler_signal, b_spline
 from coefficients import unweighted_coefficient, weighted_coefficient
 
 
+# Parameters for Modulation space
 modulation_smoothness = 1
 s = 1
 epsilon = 0.1
+
+# Parameters for signal and window
 time_location = 40
 frequency = 200
+spline_order = 4
+interval_signal = [0, 1]
+interval_spline = [0, 1]
+signal = doppler_signal(time_location, frequency, interval=interval_signal)
+window = b_spline(spline_order, interval=interval_spline)
 
-signal = doppler_signal(time_location, frequency, interval=[0, 1])
-window = b_spline(4, [0, 1])
-
-alphas = [0]
+# Parameters for the transform
+alphas = [0, 0.5, 0.9]
 js = range(-10, 10)
 ks = range(-10, 10)
+
+# 3 dimensional result matrix
 result = np.zeros((len(alphas), len(ks), len(js)))
 
+# Calculation
 for alpha_index in range(len(alphas)):
     for j_index in range(len(js)):
         for k_index in range(len(ks)):
@@ -27,6 +36,8 @@ for alpha_index in range(len(alphas)):
             k = ks[k_index]
 
             result[alpha_index, k_index, j_index] = unweighted_coefficient(signal, window, j, k, epsilon, alpha)
+            # result[alpha_index, k_index, j_index] = weighted_coefficient(signal, window, j, k, epsilon, s, alpha, p)
 
 
+# Plot the result for the first alpha
 plot_matrix(result[0, :, :])
--- test_setup_parallel.py
+++ test_setup_parallel.py
@@ -6,21 +6,28 @@ from plots import plot_matrix, plot_callable
 from functions import doppler_signal, b_spline
 from coefficients import unweighted_coefficient, weighted_coefficient
 
+
+# Parameters for Modulation space
 modulation_smoothness = 1
 s = 1
 epsilon = 0.1
+
+# Parameters for signal and window
 time_location = 40
 frequency = 200
+spline_order = 4
+interval_signal = [0, 1]
+interval_spline = [0, 1]
+signal = doppler_signal(time_location, frequency, interval=interval_signal)
+window = b_spline(spline_order, interval=interval_spline)
 
-signal = doppler_signal(time_location, frequency, interval=[0, 1])
-window = b_spline(4, [0, 1])
-
+# Parameters for the transform
 alphas = [0, 0.5, 0.9]
 js = range(-10, 10)
 ks = range(-10, 10)
-result_dict = {}
 
 
+# Get the coefficients. Uncomment the version you want to use. Also returns the parameter alpha to use for logging
 def get_coefficients(alpha):
     coefficients = np.zeros((len(ks), len(js)))
     for j_index in range(len(js)):
@@ -28,21 +35,22 @@ def get_coefficients(alpha):
             j1 = js[j_index]
             k1 = ks[k_index]
             coefficients[k_index][j_index] = unweighted_coefficient(signal, window, j1, k1, epsilon, alpha)
-    return alpha, coefficients
+            # coefficients[k_index][j_index] = weighted_coefficient(signal, window, j1, k1, epsilon, s, alpha, p)
+    return coefficients, alpha
 
 
-def log_result(coefs):
-    result_dict[coefs[0]] = coefs[1]
+# Write the results to the result dictionary. Different function to use as callback
+result_dict = {}
+def log_result(coefficients):
+    result_dict[coefficients[1]] = coefficients[0]
 
 
 if __name__ == '__main__':
     pool = mp.Pool(processes=mp.cpu_count() - 1)
     for a in alphas:
-        pool.apply_async(get_coefficients, args=(a, ), callback=log_result)
+        pool.apply_async(get_coefficients, args=(a,), callback=log_result)
     pool.close()
     pool.join()
-    # for alpha in alphas:
-    #     get_coefficients(alpha)
 
     for a in alphas:
         plot_matrix(result_dict[a])
